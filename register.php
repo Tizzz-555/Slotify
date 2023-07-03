d582417777398fd7041eb7df04b497a4b3242134
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-03 19:21:01 +0200

    4.30

--- register.php
+++ register.php
@@ -23,6 +23,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Welcome to Slotify!</title>
   <link rel="stylesheet" type="text/css" href="assets/css/register.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+  <script src="assets/js/register.js"></script>
 </head>
 <body>
   <div id="background">
