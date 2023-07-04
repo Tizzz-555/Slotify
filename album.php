bb515de7b92baef000cbd58203432e422e627f3a
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-04 21:32:15 +0200

    9.77

--- album.php
+++ album.php
@@ -15,15 +15,32 @@ $artist = $album->getArtist();
 ?>
 
 <div class = 'entityInfo' >
+
 	<div class = 'leftSection' >
-		<img src = "<?php echo $album->getArtworkPath(); ?>" >
+		<img src = "<?php echo $album->getArtworkPath(); ?>">
 	</div>
 
 	<div class = 'rightSection' >
 		<h2><?php echo $album->getTitle(); ?></h2>
-		<span>By <?php echo $artist->getName(); ?></span>
+		<p>By <?php echo $artist->getName(); ?></p>
+    <p><?php echo $album->getNumberOfSongs(); ?> songs</p>
 	</div>
-  
+
+</div>
+
+<div class = 'tracklistContainer' >
+	<ul class = 'tracklist' >
+		
+    <?php 
+		$songIdArray = $album->getSongIds();
+    
+		foreach($songIdArray as $songId) {
+      echo $songId . "<br>";
+    }
+
+    ?>
+
+  </ul>
 </div>
 
 <?php include("includes/footer.php"); ?>
--- includes/classes/Album.php
+++ includes/classes/Album.php
@@ -11,7 +11,7 @@
  			$this->con = $con;
  			$this->id = $id;
 
- 			$albumQuery = mysqli_query($this->con, "SELECT * FROM albums WHERE id = '$this->id'");
+ 			$albumQuery = mysqli_query($this->con, "SELECT * FROM Albums WHERE id = '$this->id'");
 			$album = mysqli_fetch_array($albumQuery);
 
  			$this->title = $album['title'];
@@ -25,6 +25,10 @@
       return $this->title; 
     }
 
+ 		public function getArtworkPath() { 
+      return $this->artworkPath; 
+    }
+
  		public function getArtist() { 
       return new Artist($this->con, $this->artistId); 
     }
@@ -33,9 +37,22 @@
       return $this->genre; 
     }
 
-    public function getArtworkPath() { 
-      return $this->artworkPath; 
-    }
+ 		public function getNumberOfSongs() {
+ 			$query = mysqli_query($this->con, "SELECT * FROM Songs WHERE album ='$this->id'");
+ 			return mysqli_num_rows($query);
+ 		}
+    
+ 		public function getSongIds() {
+ 			$query = mysqli_query($this->con, "SELECT id FROM Songs WHERE album='$this->id' ORDER BY albumORDER ASC");
+
+ 			$array = array();
+
+ 			while($row = mysqli_fetch_array($query)) {
+ 				array_push($array, $row['id']);
+ 			}
+
+ 			return $array;
+ 		}
  	}
 
  ?>
