dfbb9954e59a68bde7868aa9a9c1eb9d6e6b1007
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-04 11:38:03 +0200

    5.46

--- index.php
+++ index.php
@@ -62,7 +62,13 @@
           </div>
           <div class="playbackBar">
             <span class="progressTime current">0.00</span>
-            <div class="progressBar"></div>
+
+            <div class="progressBar">
+              <div class="progressBarBg">
+                <div class="progress"></div>
+              </div>
+            </div>
+
             <span class="progressTime remaining">0.00</span>
           </div>
 
