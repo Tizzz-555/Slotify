8e731bd408d1e22c4bc7cd8ed921b3779a62ecf5
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-04 13:07:16 +0200

    5.53

--- index.php
+++ index.php
@@ -28,9 +28,35 @@
 
       <div id="navBarContainer">
         <nav class="navBar">
+
           <a href="index.php" class="logo">
             <img src="assets/images/icons/logo.png" alt="">
           </a>
+
+          <div class="group">
+
+            <div class="navItem">
+              <a href="search.php" class="navItemLink">Search</a>
+            </div>
+
+          </div>
+
+          <div class="group">
+
+            <div class="navItem">
+              <a href="browse.php" class="navItemLink">Browse</a>
+            </div>
+
+            <div class="navItem">
+              <a href="yourMusic.php" class="navItemLink">Your Music</a>
+            </div>
+
+            <div class="navItem">
+              <a href="profile.php" class="navItemLink">Mattia Beccari</a>
+            </div>
+            
+
+          </div>
         </nav>
       </div>
 
