2219c0724091b86332d5464130bc2167fb41d3ec
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-05 10:39:26 +0200

    auto play

--- includes/header.php
+++ includes/header.php
@@ -20,11 +20,18 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Welcome to Slotify!</title>
   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <script src="assets/js/script.js"></script>
 </head>
 
 <body>
+
+  <script>
+    var audioElement = new Audio();
+    audioElement.setTrack("assets/music/bensound-acousticbreeze.mp3");
+    audioElement.audio.play();
+  </script>
 
   <div id="mainContainer">
     
