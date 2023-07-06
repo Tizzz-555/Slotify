3c2b362676a1a9dffd669bc6307042b18e1d523b
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-06 11:16:27 +0200

    display time remaining label

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,11 @@ function Audio() {
   this.currentlyPlaying; // Property to store the currently playing track (initially undefined)
   this.audio = document.createElement("audio"); // Creating an HTML audio element using document.createElement
 
+  this.audio.addEventListener("canplay", function () {
+    // 'this' refers to the object that the event was called on
+    $(".progressTime.remaining").text(this.duration);
+  });
+
   this.setTrack = function (track) {
     this.currentlyPlaying = track;
     this.audio.src = track.path; // Set the source of the audio element to the provided src
