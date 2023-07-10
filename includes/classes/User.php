6f271de7ff0b1edbfe31382bf3832a4f86707ae0
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-10 17:02:24 +0200

    create User class

---
+++ includes/classes/User.php
@@ -0,0 +1,12 @@
+<?php
+  class User {
+
+    private $con;
+    private $username;
+
+    public function __construct($con, $username) {
+      $this->con = $con;
+      $this->username = $username;
+    }
+}
+?>
