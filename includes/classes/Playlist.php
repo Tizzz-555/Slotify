1b945839c6f9e27a47e65b8e48dfd308e6ae7870
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-10 19:15:20 +0200

    display playlist songs

--- includes/classes/Playlist.php
+++ includes/classes/Playlist.php
@@ -35,5 +35,18 @@
       $query = mysqli_query($this->con, "SELECT songId FROM playlistSongs WHERE playlistId='$this->id'");
       return mysqli_num_rows($query);
     }
+
+    public function getSongIds() {
+			$query = mysqli_query($this->con, "SELECT songId FROM playlistSongs WHERE playlistId='$this->id' ORDER BY playlistOrder ASC");
+			// Query the database using the private 'id' property to retrieve the IDs of all songs associated with the album, ordered by 'albumORDER' column
+
+			$array = array();   // Create an empty array
+
+			while ($row = mysqli_fetch_array($query)) {
+				array_push($array, $row['songId']);   // Add each song ID to the array
+			}
+
+			return $array;   // Return the array of song IDs
+		}
 }
 ?>
