9b00a5cd0840584f7607dd59640926db2c512ab4
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-07-11 15:31:31 +0200

    settings link on nav menu

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,18 @@
 <?php
   include("includes/config.php");
+  include("includes/classes/User.php");
   include("includes/classes/Artist.php");
   include("includes/classes/Album.php");
   include("includes/classes/Song.php");
+  include("includes/classes/Playlist.php");
 
 
   //session_destroy(); LOGOUT
 
   if(isset($_SESSION['userLoggedIn'])) { // Check if the 'userLoggedIn' key is set in the $_SESSION array
-    $userLoggedIn = $_SESSION['userLoggedIn']; // Assign the value of $_SESSION['userLoggedIn'] to $userLoggedIn
-    echo "<script>userLoggedIn = '$userLoggedIn';</script>";
+    $userLoggedIn = new User($con, $_SESSION['userLoggedIn']); // Assign the value of $_SESSION['userLoggedIn'] to $userLoggedIn
+    $username = $userLoggedIn->getUsername();
+    echo "<script>userLoggedIn = '$username';</script>";
   }
   else {
     header("Location: register.php"); // Redirect the user to the "register.php" page if the 'userLoggedIn' key is not set
